4524c5e78c618117c1efa94b86688759e95f72ec
rbrandon87 <rbrandon87@gmail.com>
2018-05-25 12:46:01 -0400

    Week 1 updates
    Exercise Completed

--- Week1ExerciseScript.R
+++ Week1ExerciseScript.R
@@ -1,1 +1,7 @@
-1+1
+library("ggplot2")
+ggplot(iris) + aes(x = Species, y = Sepal.Length) + geom_boxplot()
+hist(iris$Petal.Width, col = "green", xlab = "Petal Width", ylab = "Total #", breaks=15)
+cor(iris$Sepal.Length, iris$Sepal.Width)
+cor(iris$Petal.Length,iris$Petal.Width)
+summary(iris)
+a[order(a)]
